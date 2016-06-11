06f9acdb964b039ac81f14aa3f861d964b06de78
Casey <Code@casey-macbook.local>
2016-06-11 11:01:04 -0700

    update

--- PermData/ViewController.swift
+++ PermData/ViewController.swift
@@ -13,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSUserDefaults.standardUserDefaults().setObject("Casey", forKey: "name")
+        
+        let userName = NSUserDefaults.standardUserDefaults().objectForKey("name")! as! String
+        
+        print(userName)
+        
+        
+        let arr = [1,2,3,4]
+        
+        NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "array")
+        
+        let retArr = NSUserDefaults.standardUserDefaults().objectForKey("array")! as! NSArray
+        
+        for x in retArr{
+           
+            print(x)
+        
+        }
     }
 
     override func didReceiveMemoryWarning() {
